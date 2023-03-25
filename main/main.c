9ffa806214ac6d0e48548443cb0ceeac820e5b96
thanhlongntl525 <thanhlongntl525@gmail.com>
2023-03-25 22:25:44 +0700

    first commit

---
+++ main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#define BLINK_GPIO 2
+void app_main()
+{
+ gpio_pad_select_gpio(BLINK_GPIO);
+
+ gpio_set_direction(BLINK_GPIO,
+GPIO_MODE_OUTPUT);
+ while(1) {
+
+ gpio_set_level(BLINK_GPIO, 0);
+ vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+ gpio_set_level(BLINK_GPIO, 1);
+ vTaskDelay(1000 / portTICK_PERIOD_MS);
+ }
+}
